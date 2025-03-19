7d1f237318477d76ab6f4db24598b26bfdbe13e6
Ritesh jha <jhar27018@gmail.com>
2025-03-19 16:25:07 +0530

    Add IframeRedLight component to embed external content

---
+++ src/app/red-light-iframe/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useEffect } from "react";
+
+const IframeRedLight = () => {
+  const [width, setWidth] = React.useState<number>(400);
+  const [height, setHeight] = React.useState<number>(400);
+  useEffect(() => {
+    if (window === null) return;
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }, [window]);
+  return (
+    <div className="w-[100dvw] h-[100dvh]">
+      <iframe
+        src="https://redlight-one.vercel.app/"
+        width={width}
+        height={height}
+      ></iframe>
+    </div>
+  );
+};
+
+export default IframeRedLight;
