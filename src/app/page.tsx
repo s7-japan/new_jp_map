04585e49d9aa742218f928743abfb7028faa9fe4
upgrace-in <thedesiretree47@gmail.com>
2025-03-20 09:27:30 +0530

    Redirection URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "@/components/Header";
 import Direction from "@/components/Direction";
 import dynamic from "next/dynamic";
@@ -8,7 +8,20 @@ const Map = dynamic(() => import("@/components/Map"), {
   ssr: false,
 });
 import EventCalendar from "@/components/EventCalendar";
+
+import { useSearchParams, useRouter } from "next/navigation";
+
 const MapPage = () => {
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const redirectPath = searchParams.get("redirect");
+
+  useEffect(() => {
+    if (redirectPath)
+      router.push(`/${redirectPath}`)
+  }, [redirectPath, router])
+
   return (
     <div className="bg-white w-full h-full">
       <Header />
