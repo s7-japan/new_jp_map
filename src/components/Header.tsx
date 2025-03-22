21c1ac50974811f3cbfd273bb41564ae7a9e93d9
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-03-22 14:54:44 +0530

    Adjusted the text in the header of the minimap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
     <div className="h-[90px] rounded-bl-4xl rounded-br-4xl shadow-lg flex flex-col justify-center items-center fixed w-full bg-white z-[50] MyCustomFont">
-      <strong className="font-normal">
+      <strong className="font-normal text-xl">
         CI<span className="text-[#ff0000]">R</span>CUIT JOURN
         <span className="text-[#ff0000]">E</span>Y
       </strong>
-      <p className="text-sm mt-2 MyCustomFont">イベントマップ</p>
+      <p className="text-sm mt-1 MyCustomFont">イベントマップ</p>
     </div>
   );
 };
