832343f9fc3114877280b1d47e34a35799edd439
upgrace-in <thedesiretree47@gmail.com>
2025-03-23 21:46:51 +0530

    Redirection Mechanism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
-"use client"
-import React, { useEffect, Suspense } from "react"
-import Header from "@/components/Header"
-import Direction from "@/components/Direction"
-import dynamic from "next/dynamic"
+"use client";
+import React, { useEffect, Suspense } from "react";
+import Header from "@/components/Header";
+import Direction from "@/components/Direction";
+import dynamic from "next/dynamic";
 const Map = dynamic(() => import("@/components/Map"), {
   ssr: false,
-})
-import EventCalendar from "@/components/EventCalendar"
+});
+import EventCalendar from "@/components/EventCalendar";
+import { useRouter, useSearchParams } from "next/navigation";
 
+// Create a separate component that uses useSearchParams
 const RedirectHandler = () => {
-  useEffect(() => {
-    alert(window.location.href)
-  }, [])
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const redirectPath = searchParams.get("liff.state");
 
-  return null
-}
+  useEffect(() => {
+    if(redirectPath){
+      // console.log(redirectPath)
+      router.push(`${redirectPath}`)
+    }
+  }, [redirectPath]);
+
+  return null;
+};
 
 const MapPage = () => {
   return (
