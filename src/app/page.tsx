265c882318a9712f3211feb6bb16c2c234cd92c9
upgrace-in <thedesiretree47@gmail.com>
2025-03-23 21:22:05 +0530

    Testing redirection mechanism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
-"use client";
-import React, { useEffect, Suspense } from "react";
-import Header from "@/components/Header";
-import Direction from "@/components/Direction";
-import dynamic from "next/dynamic";
+"use client"
+import React, { useEffect, Suspense } from "react"
+import Header from "@/components/Header"
+import Direction from "@/components/Direction"
+import dynamic from "next/dynamic"
 const Map = dynamic(() => import("@/components/Map"), {
   ssr: false,
-});
-import EventCalendar from "@/components/EventCalendar";
-import { useRouter, useSearchParams } from "next/navigation";
+})
+import EventCalendar from "@/components/EventCalendar"
 
-// Create a separate component that uses useSearchParams
 const RedirectHandler = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const redirectPath = searchParams.get("redirect");
-
   useEffect(() => {
-    if (redirectPath) router.push(`/${redirectPath}`);
-  }, [redirectPath, router]);
+    if(window){
+      alert(window.location.href)
+    }
+  }, [window])
 
-  return null;
-};
+  return null
+}
 
 const MapPage = () => {
   return (
