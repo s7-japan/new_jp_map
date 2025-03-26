1ca5f293981ab97ba1c33a52cfeedd340ce807b2
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-03-26 07:13:35 +0530

    error resolve

--- src/app/minimap/page.tsx
+++ src/app/minimap/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Header from "../../components/Header";
-import Direction from "../../components/Direction";
 import dynamic from "next/dynamic";
 import BottomNav from "@/components/BottomNav";
 const Map = dynamic(() => import("../../components/Map"), {
