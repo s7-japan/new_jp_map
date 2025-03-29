0e770a88d4ebda460a0ad08133198b0c430623c4
Ritesh jha <jhar27018@gmail.com>
2025-03-29 12:15:13 +0530

    style: update EventMapHeader layout and text for improved presentation

--- src/components/EventMapHeader.tsx
+++ src/components/EventMapHeader.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from "react";
 
 const EventMapHeader = () => {
@@ -17,13 +16,13 @@ const EventMapHeader = () => {
   }, [prevScrollPos]);
 
   return (
-    <div className="fixed h-[65px] top-0 left-0 right-0 bg-white rounded-b-3xl text-center shadow-xl z-[1000] flex-col justify-between items-center  MyCustomFont pb-3">
+    <div className=" bg-white rounded-b-3xl text-center shadow-xl z-[1000] flex-col justify-between items-center  MyCustomFont pb-3">
       <strong className="font-normal text-[1.6rem]">
         <span className="text-[#ff0000]">E</span>VENT CAL
         <span className="text-[#ff0000]">E</span>NDAR
       </strong>
-      <p className="font-bold text-[12px] font-[JPFonts] mb-5">
-        イベントカレンダー
+      <p className="font-light text-[12px] font-[JPFonts] mb-5">
+        サーキットジャーニー
       </p>
     </div>
   );
