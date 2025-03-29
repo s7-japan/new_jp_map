ccb510dcdd9138653e0d21d3146038bcba9b29aa
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-03-29 11:46:43 +0530

    Added the boldness in the humbergermenu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -30,7 +30,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://www.suzukacircuit.jp/f1/ticket/index.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_ticket&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.suzukacircuit.jp/f1/ticket/index.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_ticket&utm_campaign=f1&utm_content=20250331"
+              >
                 チケット
               </a>
             </li>
@@ -42,7 +45,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://www.suzukacircuit.jp/f1/access/index.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_access&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.suzukacircuit.jp/f1/access/index.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_access&utm_campaign=f1&utm_content=20250331"
+              >
                 アクセス
               </a>
             </li>
@@ -54,7 +60,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://miniapp.line.me/2007078799-0oWyrXee/eventcalender">
+              <a
+                className="font-bold"
+                href="https://miniapp.line.me/2007078799-0oWyrXee/eventcalender"
+              >
                 イベント情報
               </a>
             </li>
@@ -66,7 +75,9 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://goods.mobilitystation.jp/">オンラインショップ</a>
+              <a className="font-bold" href="https://goods.mobilitystation.jp/">
+                オンラインショップ
+              </a>
             </li>
             <li
               onClick={() => {
@@ -76,7 +87,12 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://ticket.mobilitystation.jp/">会員限定</a>
+              <a
+                className="font-bold"
+                href="https://ticket.mobilitystation.jp/"
+              >
+                会員限定
+              </a>
             </li>
             <li
               onClick={() => {
@@ -86,7 +102,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://www.suzukacircuit.jp/f1/dazn.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_dazn&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.suzukacircuit.jp/f1/dazn.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_dazn&utm_campaign=f1&utm_content=20250331"
+              >
                 オンライン観戦
               </a>
             </li>
@@ -98,7 +117,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-link__item"
             >
-              <a className="font-[600]" href="https://www.suzukacircuit.jp/contact_s/?utm_medium=lineeoa&utm_source=ln_miniapp_menu_contact&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.suzukacircuit.jp/contact_s/?utm_medium=lineeoa&utm_source=ln_miniapp_menu_contact&utm_campaign=f1&utm_content=20250331"
+              >
                 お問い合わせ
               </a>
             </li>
@@ -112,7 +134,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sub__item"
             >
-              <a className="font-[600]" href="https://www.honda-ml.co.jp/legal/line/terms.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_terms&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.honda-ml.co.jp/legal/line/terms.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_terms&utm_campaign=f1&utm_content=20250331"
+              >
                 利用規約
               </a>
             </li>
@@ -124,7 +149,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sub__item"
             >
-              <a className="font-[600]" href="https://www.honda-ml.co.jp/privacy/?utm_medium=lineeoa&utm_source=ln_miniapp_menu_privacy&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.honda-ml.co.jp/privacy/?utm_medium=lineeoa&utm_source=ln_miniapp_menu_privacy&utm_campaign=f1&utm_content=20250331"
+              >
                 プライバシーポリシー
               </a>
             </li>
@@ -136,7 +164,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sub__item"
             >
-              <a className="font-[600]" href="https://www.honda-ml.co.jp/legal/line/credit.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_credit&utm_campaign=f1&utm_content=20250331">
+              <a
+                className="font-bold"
+                href="https://www.honda-ml.co.jp/legal/line/credit.html?utm_medium=lineeoa&utm_source=ln_miniapp_menu_credit&utm_campaign=f1&utm_content=20250331"
+              >
                 クレジットライセンス
               </a>
             </li>
@@ -150,7 +181,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sns__item"
             >
-              <a className="font-[600]" href="https://www.instagram.com/f1japanesegp/">
+              <a
+                className="font-bold"
+                href="https://www.instagram.com/f1japanesegp/"
+              >
                 <img src="/FooterIcons/footer_sns_01.svg" alt="Instagram" />
               </a>
             </li>
@@ -162,7 +196,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sns__item"
             >
-              <a className="font-[600]" href="https://www.facebook.com/suzukacircuit/">
+              <a
+                className="font-bold"
+                href="https://www.facebook.com/suzukacircuit/"
+              >
                 <img src="/FooterIcons/footer_sns_02.svg" alt="Facebook" />
               </a>
             </li>
@@ -174,7 +211,7 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sns__item"
             >
-              <a className="font-[600]" href="https://twitter.com/suzuka_event">
+              <a className="font-bold" href="https://twitter.com/suzuka_event">
                 <img src="/FooterIcons/footer_sns_03.svg" alt="X" />
               </a>
             </li>
@@ -186,7 +223,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sns__item"
             >
-              <a className="font-[600]" href="https://www.youtube.com/channel/UCbVkZ41otxc8rCAu7_gsO6Q">
+              <a
+                className="font-bold"
+                href="https://www.youtube.com/channel/UCbVkZ41otxc8rCAu7_gsO6Q"
+              >
                 <img src="/FooterIcons/footer_sns_04.svg" alt="YouTube" />
               </a>
             </li>
@@ -198,7 +238,10 @@ const HamburgerMenu = ({ isOpen, onClose }) => {
               }}
               className="footer-sns__item"
             >
-              <a className="font-[600]" href="https://www.tiktok.com/@suzuka.circuit.park?lang=ja-JP">
+              <a
+                className="font-bold"
+                href="https://www.tiktok.com/@suzuka.circuit.park?lang=ja-JP"
+              >
                 <img src="/FooterIcons/footer_sns_05.svg" alt="TikTok" />
               </a>
             </li>
