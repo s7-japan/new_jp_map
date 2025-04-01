1d9bcfac16c021df811f725c5b95263ad0c97435
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-04-02 00:46:59 +0530

    Adjusted the z index of the humberger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="h-[65px] rounded-bl-2xl rounded-br-2xl flex flex-col justify-between items-center fixed w-full bg-white z-[100] formula1 shadow-xl pb-5">
+    <div className="h-[65px] rounded-bl-2xl rounded-br-2xl flex flex-col justify-between items-center fixed w-full bg-white z-[1500] formula1 shadow-xl pb-5">
       <strong className="font-normal text-[1.6rem]">
         CI<span className="text-[#ff0000]">R</span>CUIT JOURN
         <span className="text-[#ff0000]">E</span>Y
