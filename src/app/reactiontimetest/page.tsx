594aa15f5090b27ef4f03f8425a83c3ec240b6ff
Prince Gupta <princegupta@Princes-MacBook-Pro.local>
2025-04-02 22:52:45 +0530

    updated 002

--- src/app/reactiontimetest/page.tsx
+++ src/app/reactiontimetest/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 const Page = () => {
-  useEffect(() => {
-    // Redirect to the specified URL when the page loads
-    window.location.href = "https://redlight-one.vercel.app/";
-  }, []);
+  // useEffect(() => {
+  //   // Redirect to the specified URL when the page loads
+  //   window.location.href = "https://redlight-one.vercel.app/";
+  // }, []);
 
   return null; // No need to render anything since the user is redirected
 };
