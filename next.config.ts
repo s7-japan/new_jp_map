a7cf7d1bc4731c95ce8fd5f1cad50c4bd7944c40
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-04-03 20:47:45 +0530

    Fixed the focusing issue in iphone

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,6 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://d2ibu2ug0mt5qp.cloudfront.net",
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data:", // Already correct for images
-              "font-src 'self'",
-              `connect-src 'self' data: https://liff.line.me ${
-                isDev ? "ws://localhost:3000" : ""
-              }`, // Allow data: URLs and LIFF domains
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during builds
   },
--- src/app/reactiontimetest/page.tsx
+++ src/app/reactiontimetest/page.tsx
@@ -3,13 +3,36 @@ import BottomFooter from "@/components/BottomFooter";
 import React from "react";
 
 const Page = () => {
+  // const triggerURL = (url: string) => {
+  //   const iframe = document.createElement("iframe");
+  //   iframe.style.display = "none";
+  //   iframe.src = url;
+
+  //   document.body.appendChild(iframe);
+
+  //   console.log("working");
+  //   setTimeout(() => {
+  //     document.body.removeChild(iframe);
+  //   }, 1000);
+  // };
+
+  // useEffect(() => {
+  //   triggerURL(
+  //     "https://app.dialogone.jp/v1/linelogin/auth/414a525aca27bd66?index=20250329appRTTcircuit"
+  //   );
+  // }, []);
+
   return (
     <div
       style={{
         height: "100vh",
         width: "100vw",
+        margin: 0,
+        padding: 0,
+        position: "relative",
         display: "flex",
         flexDirection: "column",
+        overflow: "hidden", // Prevent scrolling issues
       }}
     >
       <iframe
