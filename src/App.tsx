46000a7256b15f6481dd8e7f82b90b7a5cc9d22c
upgrace-in <thedesiretree47@gmail.com>
2025-04-05 20:12:30 +0530

    On the same #2

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import Header from "@/components/Header";
 import Direction from "@/components/Direction";
 import Map from "@/components/Map";
 import EventCalendar from "@/components/EventCalendar";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    window.location.href = "https://redlight-black.vercel.app/"
+  }, []);
+
   return (
     <div className="bg-white">
       <Header />
